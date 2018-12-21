21217576b284253dd677c6795c3bc5d6176abcf1
sungheonpark <sungheonpark@snu.ac.kr>
2018-12-21 17:02:58 +0900

    Update download_models.sh

--- download_models.sh
+++ download_models.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 "Download RN models"
-wget -O models/RN/net_iter_100000.caffemodel https://www.dropbox.com/s/hxm5juwvrtn4ga4/RN_net_iter_100000.caffemodel?dl=1
+wget -O models/RN/net_iter_100000.caffemodel https://www.dropbox.com/s/xv15fneyce2wnzo/RN_net_iter_100000.caffemodel?dl=1
 "Download RN Drop models"
-wget -O models/RN_drop/net_iter_100000.caffemodel https://www.dropbox.com/s/f0ou476d4j1kryi/RN_drop_net_iter_100000.caffemodel?dl=1
+wget -O models/RN_drop/net_iter_100000.caffemodel https://www.dropbox.com/s/hj6xc821599jxe6/RN_drop_net_iter_100000.caffemodel?dl=1
