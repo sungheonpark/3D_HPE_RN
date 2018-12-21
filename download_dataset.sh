024b20ad5482218f5dcefe66c87f36c8fd7cc551
sungheonpark <sungheonpark@snu.ac.kr>
2018-12-21 17:03:45 +0900

    Update download_dataset.sh

--- download_dataset.sh
+++ download_dataset.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 echo "Downloading training data"
-wget -O data_train.tar https://www.dropbox.com/s/003y2dd8j1m70z8/data_train.tar?dl=1
+wget -O data_train.tar https://www.dropbox.com/s/1cta5s1v49sqyui/data_train.tar?dl=1
 echo "Downloading test data"
-wget -O data_test.tar https://www.dropbox.com/s/dw0v3ur3f36rnf8/data_test.tar?dl=1
+wget -O data_test.tar https://www.dropbox.com/s/qxztplajve33qkg/data_test.tar?dl=1
 echo "Extracting training data"
 tar xf data_train.tar
 echo "Extracting test data"
